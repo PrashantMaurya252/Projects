3b932f9b4e414d9a214215e420f426ee5dd3d4bb
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-07-18 20:22:03 +0530

    My Third Commit

---
+++ Arrays/Array1.js
@@ -0,0 +1,24 @@
+var Heroes=["SuperMan","SpiderMan","BatMan","Thor"]
+console.log(Heroes[3])
+
+var Marvel=new Array(4)
+Marvel[0]="Hulk"
+Marvel[1]="Black Widow"
+Marvel[2]="Black Panther"
+Marvel[3]="Captain America"
+// console.log(Marvel)
+// console.log(Marvel.pop())
+// console.log(Marvel)
+// console.log(Marvel.push("Dr Strange"))
+// console.log(Marvel)
+// console.log(Marvel.shift())
+// console.log(Marvel)
+// console.log(Marvel.unshift("Iron Man"))
+// console.log(Marvel)
+// console.log(Heroes.concat(Marvel))
+// console.log(Heroes.join(Marvel))
+// var spd=Heroes.splice(2,1,"Iron Man","Hulk")
+// console.log(Heroes)
+// console.log(spd)
+console.log(Marvel.reverse())
+console.log(Marvel.indexOf("Hulk"))
---
+++ Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(Heroes.join(Marvel))
